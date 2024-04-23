494ca009c82ad6facbf37301711b719c236d0db3
EngJao89 <eng_joao@outlook.com>
2024-04-23 00:07:21 -0300

    feat(iwa06-axios): implementa axios para consumo de serviço

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,9 +1,39 @@
+import {useEffect, useState} from 'react';
 import logo from '../../assets/Logo.png';
 import bg from '../../assets/background.png';
 import {SelectList} from '../../components/SelectList';
+import {
+  CityProps,
+  getCityByNameService,
+} from '../../services/getCityByNameService';
 import {Container, Content, ImageCont, SubTitle, TextB, Title} from './styles';
 
 export function Search() {
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState<CityProps[]>([]);
+
+  const {handleChanceCity} = useCity();
+
+  async function getCities(city: string) {
+    setIsLoading(true);
+
+    const response = await getCityByNameService(city);
+
+    setCities(response);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if (search.trim().length === 0) {
+      return;
+    }
+
+    const debounce = setTimeout(() => getCities(search), 500);
+
+    return () => clearInterval(debounce);
+  }, [search]);
+
   return (
     <Container source={bg} resizeMode="cover">
       <ImageCont source={logo} resizeMode="cover" />
@@ -14,7 +44,13 @@ export function Search() {
         </Title>
         <SubTitle>Escolha um local para ver a previsão do tempo</SubTitle>
 
-        <SelectList placeholder="Buscar local" />
+        <SelectList
+          data={cities}
+          onChange={setSearch}
+          isLoading={isLoading}
+          onPress={handleChanceCity}
+          placeholder="Buscar local"
+        />
       </Content>
     </Container>
   );
